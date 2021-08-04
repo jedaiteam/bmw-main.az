61a56b8a9c996184df7535ada776b2cc3c45aa7e
Nasir <movlamovnasir2000@gmail.com>
2021-08-04 09:03:50 +0400

    sitemap-com

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://bmwpartsbaku.az',
+    siteUrl: 'https://bmwpartsbaku.az',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
--- pages/Katalog.jsx
+++ pages/Katalog.jsx
@@ -3,10 +3,10 @@ import BmwPartsCard from '../components/BmwPartsCard'
 import BodyBanner from '../components/BodyBanner'
 import Layout from '../components/Layout'
 import Head from 'next/head'
-const Katalog = () => {
+const katalog = () => {
     return (
       <Layout>
-                      <Head>
+        <Head>
         <title>BMW üçün ehtiyat Hissələri</title>
         <meta name="description" content="BMW ehtiyyat hissələri"/>
         <link rel="icon" href="../uploads/bmw.svg" />
