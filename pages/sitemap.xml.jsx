b8d65fdaa2ba66956eabc338fee34efd7e6ef88d
Nasir <movlamovnasir2000@gmail.com>
2021-08-12 15:56:02 +0400

    change

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -21,7 +21,7 @@ export const getServerSideProps = async ({ res }) => {
   const staticPages = fs
   .readdirSync({
     development: 'pages',
-    production: '../../../../../../../../../../../../',
+    production: '/',
   }[process.env.NODE_ENV])
   .filter((staticPage) => {
     return ![
