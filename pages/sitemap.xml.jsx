bac83173670885e1667c447b96dc97333e1d4cd8
Nasir <movlamovnasir2000@gmail.com>
2021-08-12 15:20:31 +0400

    change

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -39,6 +39,7 @@ export const getServerSideProps = async ({ res }) => {
     return `${baseUrl}/${staticPagePath}`;
   });
   
+  
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
