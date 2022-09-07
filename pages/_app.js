0bc6cbf8957b800259834d2bb58e05ed39189aca
nasirmovlamov <60513066+nasirmovlamov@users.noreply.github.com>
2022-09-07 18:35:17 +0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
       f.parentNode.insertBefore(j, f);
     })(window, document, "script", "dataLayer", "GTM-56ND7NW");
 
-    console.log("google analytics elave edildi');
+    console.log("google analytics elave edildi");
   });
   return (
     <>
